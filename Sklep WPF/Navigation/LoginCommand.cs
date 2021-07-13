6db2cc87ec16d4be7ebabca0d9103ecf2a890ece
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-07-13 02:21:18 +0200

    create LoginCommand
    created LoginCommand file
    modified LoginViewModel

---
+++ Sklep WPF/Navigation/LoginCommand.cs
@@ -0,0 +1,28 @@
+﻿using Sklep_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sklep_WPF.Navigation
+{
+    class LoginCommand : CommandBase
+    {
+        private readonly LoginViewModel _viewModel;
+        private readonly Navigate _navigate;
+
+        public LoginCommand(LoginViewModel viewModel, Navigate navigate)
+        {
+            _viewModel = viewModel;
+            _navigate = navigate;
+        }
+        public override void Execute(object p)
+        {
+            MessageBox.Show("Email: " + _viewModel.Email);
+
+            _navigate.CurrentPage = new UserViewModel();
+        }
+    }
+}
--- Sklep WPF/ViewModel/LoginViewModel.cs
+++ Sklep WPF/ViewModel/LoginViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Sklep_WPF.Navigation;
 using System.Windows.Input;
 using System.Windows;
+using Sklep_WPF.Model;
 
 namespace Sklep_WPF.ViewModel
 {
@@ -14,21 +15,6 @@ namespace Sklep_WPF.ViewModel
 
     class LoginViewModel : ViewModelBase
     {
-        private readonly Navigate _navigate;
-
-        public ViewModelBase CurrentPage => _navigate.CurrentPage;
-
-        public LoginViewModel(Navigate navigate)
-        {
-            _navigate = navigate;
-            _navigate.CurrentPageChanged += OnCurrentPageChanged;
-        }
-
-        private void OnCurrentPageChanged()
-        {
-            onPropertyChanged(nameof(CurrentPage));
-        }
-
         private string _email;
         public string Email
         {
@@ -42,60 +28,15 @@ namespace Sklep_WPF.ViewModel
 
         public SecureString SecurePassword { private get; set; }
 
-
-        //                                                                                  Login Button
-        private ICommand _login;
-        public ICommand Login
-        {
-            get
-            {
-                return _login ?? (_login= new RelayCommand((p) =>
-                {
-
-                    if (Email != null && Email != "" && SecurePassword != null)
-                    {
-                        /*
-                        //weryfikacja danych logowania
-                        if ()
-                        {
-                            _navigate.CurrentPage = new UserViewModel();
-                        }
-                        */
-
-                        MessageBox.Show("Email: " + Email + "\nPassword: " + SecurePassword);
-                        _navigate.CurrentPage = new UserViewModel();
-                    }
-                    else 
-                    {
-                        MessageBox.Show("Pola nie mogą być puste");
-                    }
-                   
-                }, p => true));
-            }
-        }
-
-        //                                                                                  Signup Button
-        private ICommand _signup;
-        public ICommand Signup
-        {
-            get
-            {
-                return _signup ?? (_signup = new RelayCommand((p) =>
-                {
-                    _navigate.CurrentPage = new SignupViewModel();
-
-                }, p => true));
-            }
-        }
-
-
-        /*
         public ICommand Login { get; }
+        public ICommand Signup { get; }
 
         public LoginViewModel(Navigate navigate)
         {
-            Login = new NavigateCommand<UserViewModel>(navigate, () => new UserViewModel()) ;
+            Login = new LoginCommand(this, navigate);
+            Signup = new NavigateCommand<SignupViewModel>(navigate, () => new SignupViewModel());
         }
-        */
+
+
     }
 }
