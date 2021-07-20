5d780d890dc81855bb11f500e29945ea04706237
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-07-20 19:52:48 +0200

    add github button
    added github button to SettingsPage
    updated SettingsViewModel

--- Sklep WPF/ViewModel/SettingsViewModel.cs
+++ Sklep WPF/ViewModel/SettingsViewModel.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Sklep_WPF.Navigation;
+using System.Windows.Input;
 
 namespace Sklep_WPF.ViewModel
 {
     using BaseClass;
     class SettingsViewModel : ViewModelBase
     {
+       
+        private ICommand _github;
+        public ICommand Github
+        {
+            get
+            {
+                return _github?? (_github = new RelayCommand((p) =>
+                {
+                    System.Diagnostics.Process.Start("https://github.com/MrFuru18/Sklep-WPF");
 
+                }, p => true));
+            }
+        }
     }
 }
