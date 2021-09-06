05b3731dab0e418db43f60f03019bfbf33b20633
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-09-06 03:44:08 +0200

    displaying orders
    OrderHistoryPage list of orders can be diplayed now

--- Sklep WPF/ViewModel/OrderHistoryViewModel.cs
+++ Sklep WPF/ViewModel/OrderHistoryViewModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sklep_WPF.DAL.Repozytoria;
+using Sklep_WPF.Model;
 
 namespace Sklep_WPF.ViewModel
 {
     using BaseClass;
     class OrderHistoryViewModel : ViewModelBase
     {
+        public List<Order> orders { get; set; }
+
+        public OrderHistoryViewModel()
+        {
+            orders = OrderRepo.getAllOrders().Result;
+        }
+
     }
 }
