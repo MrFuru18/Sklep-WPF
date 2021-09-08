b9f65551f41447e08aa92671add45278654a6029
Wojciech Babiński <wojcbab281@student.polsl.pl>
2021-09-08 15:42:49 +0200

    bind sort by ComboBox

--- Sklep WPF/ViewModel/ShopViewModel.cs
+++ Sklep WPF/ViewModel/ShopViewModel.cs
@@ -8,28 +8,55 @@ using System.Windows.Input;
 using System.Windows;
 using System.Collections.ObjectModel;
 using Sklep_WPF.DAL.Repozytoria;
+using Sklep_WPF.CurrentSession;
+using System.ComponentModel;
 
 namespace Sklep_WPF.ViewModel
 {
     using BaseClass;
-    using Sklep_WPF.CurrentSession;
+    
 
     class ShopViewModel : ViewModelBase
     {
         private readonly CartProductStore _productStore;
-        public List<Product> products { get; set; }
+        public BindingList<Product> products { get; set; }
+        public List<string> sort { get; set; }
 
 
         public ShopViewModel(CartProductStore productStore)
         {
             _productStore = productStore;
 
-            products = ProductRepo.getAllProtucts().Result;
+            sort = new List<string>();
+            sort.Add("Cena rosnąco");
+            sort.Add("Cena malejąco");
+
+            products = new BindingList<Product>(ProductRepo.getAllProtucts().Result);
         }
 
-
-        //public string Quantity { get; set; }
-
+        private string _sortBy;
+        public string SortBy
+        {
+            get { return _sortBy; }
+            set
+            {
+                _sortBy = value;
+                if (_sortBy=="Cena malejąco")
+                {
+                    List<Product> SortedProducts = products.OrderByDescending(o => o.cena).ToList();
+                    products.Clear();
+                    foreach (var product in SortedProducts)
+                        products.Add(product);
+                }
+                if (_sortBy == "Cena rosnąco")
+                {
+                    List<Product> SortedProducts = products.OrderBy(o => o.cena).ToList();
+                    products.Clear();
+                    foreach (var product in SortedProducts)
+                        products.Add(product);
+                }
+            }
+        }
 
         private ICommand _addToCart;
         public ICommand AddToCart
@@ -41,8 +68,6 @@ namespace Sklep_WPF.ViewModel
                     Product selectedProduct = new Product();
                     selectedProduct = (Product)p;
                     _productStore.AddProduct(selectedProduct);
-                    //MessageBox.Show("Clicked on " + selectedProduct.nazwa/* + "\nQuantity: " + Quantity*/);
-                    //Quantity = null;
 
                 }, p => true));
             }
